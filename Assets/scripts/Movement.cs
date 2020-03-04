b2afc6a0eded9417b1e7e9e228103cc216cea34f
kevin downs <pyrosrppltoo@gmail.com>
2020-03-04 13:27:11 -0800

    init

---
+++ Assets/scripts/Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    public float speed;
+    private float moveInput;
+    private bool isGrounded;
+
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+public float jumpForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+
+    void Update(){
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius,whatIsGround);
+    
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space)){
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    
+    }
+}
